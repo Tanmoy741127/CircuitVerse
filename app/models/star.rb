ca9061bd808bd99de1720c48d889ef922f632119
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2023-03-07 19:12:30 +0530

    feat: model  docs added

--- app/models/star.rb
+++ app/models/star.rb
@@ -1,7 +1,28 @@
 # frozen_string_literal: true
 
+#
+# == Schema Information
+#
+# Table name: stars
+#
+#  id          :bigint           not null, primary key
+#  user_id     :bigint
+#  project_id  :bigint
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_stars_on_project_id  (project_id)
+#  index_stars_on_user_id     (user_id)
+#  index_stars_on_user_id_and_project_id  (user_id,project_id) UNIQUE
+
 class Star < ApplicationRecord
+  # @!attribute user
+  #   @return [User] the user that starred the project
   belongs_to :user
+  # @!attribute project
+  #   @return [Project] the project that was starred
   belongs_to :project
   after_create_commit :notify_recipient
   before_destroy :cleanup_notification
@@ -9,7 +30,6 @@ class Star < ApplicationRecord
   has_noticed_notifications model_name: "NoticedNotification"
 
   private
-
     def notify_recipient
       return if user.id == project.author_id
 
